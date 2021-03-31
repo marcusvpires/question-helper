c2c1be560cda3938401f3f0c527d9bd092215d97
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-03-31 15:05:47 -0300

    Use react hooks

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,34 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./styled";
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    let repository = window.localStorage.getItem('repository')
-    if (!repository) { repository = 'Undefined' }
-    this.state = { repository: repository };
-    this.repositoryChange = this.repositoryChange.bind(this);
+export function Header() {
+  const [repository, setRepository] = useState("");
+  
+  function changeRepository(e) {
+    setRepository(e.target.value)
+    localStorage.setItem('repository', e.target.value)
   }
 
-  repositoryChange(event) {
-    this.setState({ repository: event.target.value });
-    localStorage.setItem('repository', event.target.value);
-  }
-
-  render() {
-    return (
-      <S.HeaderWrapper>
-        <S.HeaderRepository
-          type="text"
-          name="Repository name"
-          title="Repository name"
-          placeholder="Repository"
-          value={this.state.repository}
-          onChange={this.repositoryChange}
+  return (
+    <S.HeaderWrapper>
+      <S.HeaderRepository
+        type="text"
+        name="Repository name"
+        title="Repository name"
+        placeholder="Repository"
+        value={repository}
+        onChange={changeRepository}
         />
-      </S.HeaderWrapper>
-    );
-  }
+    </S.HeaderWrapper>
+  );
 }
 
 export default Header;
