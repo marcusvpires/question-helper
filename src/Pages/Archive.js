84b7b83fccda36c2d4fabcadc11bb247122d7a2a
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-04-04 21:19:03 -0300

    React Router

---
+++ src/Pages/Archive.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const ArchivePage = () => {
+
+  return (
+    <Layout>
+      
+    </Layout>
+  )
+};
+
+export default ArchivePage;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Questions from './Pages/Questions.js'
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
+import ReactDOM from "react-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Questions from "./Pages/Question.js";
+import Archive from "./Pages/Archive.js";
+
+const ReactRouter = () => {
+  return (
+    <Router>
+        <Switch>
+          <Route exact path="/">
+            <Questions />
+          </Route>
+          <Route path="/archive">
+            <Archive />
+          </Route>
+        </Switch>
+    </Router>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <Questions />
+    <ReactRouter />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 reportWebVitals();
