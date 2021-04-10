813ba08c3956240165b72e0906a0143ac5d94000
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-04-10 18:17:43 -0300

    Text imput useState

--- src/components/Forms/Text/index.js
+++ src/components/Forms/Text/index.js
@@ -3,13 +3,26 @@ import * as S from "./styled";
 
 const FormText = () => {
 
+  const [text, setText] = React.useState(() => {
+    let text = localStorage.getItem('text')
+    if (!text) { text = '' }
+    return text
+  })
+
+  const handleText = ev => {
+    const value = ev.target.value
+    localStorage.setItem('text', value)
+    setText(value)
+  }
+
   return (
     <S.TextWrapper>
       <S.TextArea
         name={'Text question input'}
         title={'Text question input'}
         placeholder='Type a text question here...'
-        onChange={ev => { localStorage.setItem('text', ev.target.value) }}
+        value={text}
+        onChange={handleText}
       />
     </S.TextWrapper>
   );
