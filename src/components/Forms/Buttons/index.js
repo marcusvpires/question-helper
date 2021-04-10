669b88cbb8a1ea8f192e420e1ea8b4c85bef3e4a
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-04-10 18:47:08 -0300

    Random

--- src/components/Forms/Buttons/index.js
+++ src/components/Forms/Buttons/index.js
@@ -1,20 +1,21 @@
 import React from "react";
+import { build } from "../../../storage/question";
 import Timer from './Timer'
 import Shortcut from './Shortcut'
 
 import * as S from "./styled";
 
-const FormButtons = ({ createQuestion, timer }) => {
+const FormButtons = ({ timer }) => {
 
   return (
     <S.BtnWrapper>
       <S.Button
         onClick={() => {
           const alternatives = ["A", "B", "C", "D", "E"];
-          createQuestion(alternatives[5 * Math.random() | 0], 'alternative')
+          build(alternatives[5 * Math.random() | 0], "alternative")
         }}
       >Random</S.Button>
-      <Shortcut createQuestion={createQuestion} />
+      <Shortcut />
       <Timer timer={timer.timer} isActive={timer.isActive} handleStart={timer.handleStart} handlePause={timer.handlePause} setTimer={timer.setTimer} />
     </S.BtnWrapper>
   );
