6f795362a4ff6db550cd7716ca19333c4ecaa1f7
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-05-06 12:54:15 -0300

    Init GA

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import reportWebVitals from "./reportWebVitals";
 import ReactDOM from "react-dom";
-import React from "react";
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ReactGA from 'react-ga';
 
 import Questions from "./Pages/Question.js";
 import Repositories from "./Pages/Repositories.js";
 import About from "./Pages/About.js";
 
 const ReactRouter = () => {
+
+  useEffect(() => {
+    ReactGA.initialize('G-0PJM86D89D');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, [])
+
   return (
     <Router>
         <Switch>
